65be0476faaa20a6450967546ae70ff0133b301c
shriwa <shriwa3429@gmail.com>
2024-02-26 21:52:22 +0530

    final

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Welcome to the backend of MERN Book Store App");
 });
 
 // mongo configure
