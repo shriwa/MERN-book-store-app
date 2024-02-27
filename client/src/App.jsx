c24e3c0b4622c5a23cf75e3cf801336b3fabf567
shriwa <shriwa3429@gmail.com>
2024-02-27 22:29:54 +0530

    deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,10 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import MyFooter from "./components/MyFooter";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <NavBar />
